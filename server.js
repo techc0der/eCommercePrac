3e4a1c976ec97a83376c5886f97cb240f4d55ab5
techc0der <sandipsuthar068@gmail.com>
2024-12-14 00:14:45 +0530

    cors added

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 const homePage = require('./Routes/home');
 const user = require('./Routes/user');
 require('./db_connect');
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/product',homePage);
 app.use('/user',user);
 const PORT = process.env.PORT  || 3000; 
